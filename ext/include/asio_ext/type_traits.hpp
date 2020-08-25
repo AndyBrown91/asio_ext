2d8c3a1fa2c8f81cda8a951c252537d8eb50520a
HungMingWu <u9089000@gmail.com>
2020-08-25 11:42:31 +0800

    Update

--- ext/include/asio_ext/type_traits.hpp
+++ ext/include/asio_ext/type_traits.hpp
@@ -10,7 +10,6 @@
 #include <type_traits>
 
 #include <asio/detail/type_traits.hpp>
-#include <asio_ext/tag.hpp>
 
 namespace asio_ext
 {
@@ -62,54 +61,5 @@ namespace asio_ext
 
     template <class Default, template <class...> class Op, class... Args>
     using detected_or = detail::detector<Default, void, Op, Args...>;
-
-    namespace detail
-    {
-        template <class Class, class... Args>
-        using tag_invoke_member_detector =
-            decltype(std::declval<Class>().tag_invoke(std::declval<Args>()...));
-    }
-
-    template <class Class, class Tag, class... Args>
-    struct has_tag_invoke_member_function
-        : ::asio_ext::is_detected<detail::tag_invoke_member_detector, Class, Tag, Args...>
-    {};
-
-    namespace detail
-    {
-        void tag_invoke();
-
-        template <class Class, class Tag, class... Args>
-        using tag_invoke_free_function_detector =
-            decltype(tag_invoke(std::declval<Class>(), std::declval<Tag>(), std::declval<Args>()...));
-    } // namespace detail
-
-    template <class Class, class Tag, class... Args>
-    struct has_tag_invoke_free_function
-        : ::asio_ext::is_detected<detail::tag_invoke_free_function_detector, Class, Tag, Args...>
-    {};
-
-    template <class Class, class Tag, class... Args>
-    struct use_tag_invoke_free_function
-        : std::conjunction<std::negation<has_tag_invoke_member_function<Class, Tag, Args...>>,
-        has_tag_invoke_free_function<Class, Tag, Args...>>
-    {};
-
-    template <class Class, class Tag, class... Args>
-    struct has_tag_invoke : std::disjunction<has_tag_invoke_member_function<Class, Tag, Args...>,
-        has_tag_invoke_free_function<Class, Tag, Args...>>
-    {};
-
-    template <class T, class E = std::exception_ptr>
-    struct is_receiver : std::conjunction<std::is_move_constructible<remove_cvref_t<T>>,
-        is_nothrow_move_or_copy_constructible<remove_cvref_t<T>>,
-        has_tag_invoke<T&&, ::asio_ext::tag::set_done_t>,
-        has_tag_invoke<T&&, ::asio_ext::tag::set_error_t, E&&>>
-    {};
-
-    template <class T, class... Vs>
-    struct is_receiver_of
-        : std::conjunction<is_receiver<T>, has_tag_invoke<T, ::asio_ext::tag::set_value_t, Vs...>>
-    {};
 
 } // namespace asio_ext
