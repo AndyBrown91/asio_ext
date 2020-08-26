1d76c2ede601b1b56ffcc4899f6dcff18dca0d3d
HungMingWu <u9089000@gmail.com>
2020-08-26 11:14:17 +0800

    Update

--- ext/include/asio_ext/when_all.hpp
+++ ext/include/asio_ext/when_all.hpp
@@ -26,126 +26,255 @@
 
 namespace asio_ext
 {
-    namespace detail
+    namespace when_all
     {
-        template <class... Senders>
-        struct when_all_op
+        namespace detail
         {
-            using senders_storage = std::tuple<Senders...>;
+            template <typename Receiver, class... Senders>
+            struct shared_state;
 
-            template <template <class...> class Tuple, template <class...> class Variant>
-            using value_types = boost::mp11::mp_unique<boost::mp11::mp_append<
-                typename asio::execution::sender_traits<Senders>::template value_types<Tuple, Variant>...>>;
-
-            template <template <class...> class Variant>
-            using error_types = boost::mp11::mp_unique<boost::mp11::mp_append<
-                typename asio::execution::sender_traits<Senders>::template error_types<Variant>...>>;
-
-            // static constexpr bool sends_done = (asio_ext::sender_traits<Senders>::sends_done || ...);
-            static constexpr bool sends_done = std::disjunction<
-                std::bool_constant<asio::execution::sender_traits<Senders>::sends_done>...>::value;
-
-            senders_storage senders_;
-
-            template <class... Tx>
-            when_all_op(Tx &&... tx) : senders_(std::forward<Tx>(tx)...) {
-            }
-
-            template <class Receiver>
-            struct operation_state
+            template <typename Receiver, class... Senders>
+            struct op_receiver
             {
-                struct shared_state;
-
-                struct op_receiver
-                {
-                    std::shared_ptr<shared_state> state_;
-                    template <class... Values>
-                    void set_value(Values &&... values) {
-                        if (state_->next_) {
-                            state_->waiting_for_--;
-                            if (state_->waiting_for_ == 0) {
-                                try {
-                                    asio::execution::set_value(*state_->next_, std::forward<Values>(values)...);
-                                    state_->next_.reset();
-                                }
-                                catch (...) {
-                                    asio::execution::set_error(*state_->next_, std::current_exception());
-                                }
+                std::shared_ptr<shared_state<Receiver, Senders...>> state_;
+                template <class... Values>
+                void set_value(Values &&... values) {
+                    if (state_->next_) {
+                        state_->waiting_for_--;
+                        if (state_->waiting_for_ == 0) {
+                            try {
+                                asio::execution::set_value(*state_->next_, std::forward<Values>(values)...);
+                                state_->next_.reset();
+                            }
+                            catch (...) {
+                                asio::execution::set_error(*state_->next_, std::current_exception());
                             }
                         }
-                        state_.reset();
                     }
+                    state_.reset();
+                }
 
-                    template <class E>
-                    void set_error(E&& e) {
-                        if (state_->next_) {
-                            asio::execution::set_error(*state_->next_, std::forward<E>(e));
-                            state_->next_.reset();
-                        }
-                        state_.reset();
+                template <class E>
+                void set_error(E&& e) {
+                    if (state_->next_) {
+                        asio::execution::set_error(*state_->next_, std::forward<E>(e));
+                        state_->next_.reset();
                     }
+                    state_.reset();
+                }
 
-                    void set_done() noexcept {
-                        if (state_->next_) {
-                            asio::execution::set_done(*state_->next_);
-                            state_->next_.reset();
-                        }
-                        state_.reset();
+                void set_done() noexcept {
+                    if (state_->next_) {
+                        asio::execution::set_done(*state_->next_);
+                        state_->next_.reset();
                     }
-                };
-                using operation_storage = std::tuple<asio::execution::connect_result_t<Senders, op_receiver>...>;
+                    state_.reset();
+                }
+            };
+
+            template <typename Receiver, class... Senders>
+            using operation_storage_t = std::tuple<
+                asio::execution::connect_result_t<
+                Senders,
+                op_receiver<Receiver, Senders...>>...>;
+
+            template <typename Receiver, class... Senders>
+            struct shared_state
+            {
+                std::optional<Receiver> next_;
+                int waiting_for_;
+                std::optional<operation_storage_t<Receiver, Senders...>> op_storage_;
+
+                shared_state(Receiver&& recv, int waiting_for)
+                    : next_(std::move(recv)), waiting_for_(waiting_for) {
+                }
+            };
+
+            template <class Receiver, class... Senders>
+            struct operation_state
+            {
+                using senders_storage = std::tuple<Senders...>;
+
+                using shared_state_t = shared_state<Receiver, Senders...>;
+
                 senders_storage senders_;
 
-                struct shared_state
-                {
-                    std::optional<Receiver> next_;
-                    int waiting_for_;
-                    std::optional<operation_storage> op_storage_;
-
-                    shared_state(Receiver&& recv, int waiting_for)
-                        : next_(std::move(recv)), waiting_for_(waiting_for) {
-                    }
-                };
-
-                std::shared_ptr<shared_state> state_;
+                std::shared_ptr<shared_state_t> state_;
 
                 template <class Rx>
                 operation_state(senders_storage&& senders, Rx&& receiver)
                     : senders_(std::move(senders)),
-                    state_(std::make_unique<shared_state>(std::forward<Rx>(receiver),
+                    state_(std::make_unique<shared_state_t>(std::forward<Rx>(receiver),
                         std::tuple_size_v<senders_storage>)) {
                 }
 
-                void start() {
+                void start() ASIO_NOEXCEPT {
                     auto sender_to_op = [this](auto &&... senders) {
-                        return operation_storage{ asio::execution::connect(std::forward<decltype(senders)>(senders),
-                                                                   op_receiver{state_})... };
+                        return operation_storage_t<Receiver, Senders...>{
+                            asio::execution::connect(
+                                std::forward<decltype(senders)>(senders),
+                                op_receiver<Receiver, Senders...>{state_})... };
                     };
 
                     state_->op_storage_.emplace(std::apply(sender_to_op, std::move(senders_)));
-                    boost::mp11::tuple_for_each(*state_->op_storage_, [](auto& op) { 
-                        asio::execution::start(op); 
+                    boost::mp11::tuple_for_each(*state_->op_storage_, [](auto& op) {
+                        asio::execution::start(op);
                     });
                 }
             };
 
-            template <class Receiver>
-            auto connect(Receiver&& receiver) {
-                return operation_state<asio_ext::remove_cvref_t<Receiver>>(
-                    std::move(senders_), std::forward<Receiver>(receiver));
+            template <class... Senders>
+            struct when_all_op
+            {
+                using senders_storage = std::tuple<Senders...>;
+
+                template <template <class...> class Tuple, template <class...> class Variant>
+                using value_types = boost::mp11::mp_unique<boost::mp11::mp_append<
+                    typename asio::execution::sender_traits<Senders>::template value_types<Tuple, Variant>...>>;
+
+                template <template <class...> class Variant>
+                using error_types = boost::mp11::mp_unique<boost::mp11::mp_append<
+                    typename asio::execution::sender_traits<Senders>::template error_types<Variant>...>>;
+
+                // static constexpr bool sends_done = (asio_ext::sender_traits<Senders>::sends_done || ...);
+                static constexpr bool sends_done = std::disjunction<
+                    std::bool_constant<asio::execution::sender_traits<Senders>::sends_done>...>::value;
+
+                senders_storage senders_;
+
+                template <class... Tx>
+                when_all_op(Tx &&... tx) : senders_(std::forward<Tx>(tx)...) {}
+
+                template <class Receiver>
+                auto connect(Receiver&& receiver) {
+                    return operation_state<asio_ext::remove_cvref_t<Receiver>, Senders...>(
+                        std::move(senders_), std::forward<Receiver>(receiver));
+                }
+            };
+        } // namespace detail
+
+        struct cpo
+        {
+            template <class S>
+            auto operator()(S&& sender) const {
+                return std::forward<asio_ext::remove_cvref_t<S>>(sender);
+            }
+
+            template <class First, class... Rest>
+            auto operator()(First&& first, Rest&&... rest) const {
+                return detail::when_all_op<
+                    asio_ext::remove_cvref_t<First>, 
+                    asio_ext::remove_cvref_t<Rest>...>(
+                    std::forward<First>(first),
+                    std::forward<Rest>(rest)...);
             }
         };
-    } // namespace detail
 
-    template <class S>
-    auto when_all(S&& sender) {
-        return std::forward<asio_ext::remove_cvref_t<S>>(sender);
-    }
+        template <typename T = cpo>
+        struct static_instance
+        {
+            static const T instance;
+        };
 
-    template <class... Senders>
-    auto when_all(Senders &&... senders) {
-        static_assert(sizeof...(Senders) > 0, "when_all must take at least 1 sender");
-        return detail::when_all_op<asio_ext::remove_cvref_t<Senders>...>(
-            std::forward<Senders>(senders)...);
+        template <typename T>
+        const T static_instance<T>::instance = {};
     }
 } // namespace asio_ext
+
+namespace asio {
+namespace execution {
+static ASIO_CONSTEXPR const asio_ext::when_all::cpo&
+      when_all = asio_ext::when_all::static_instance<>::instance;
+} // namespace execution
+} // namespace asio
+
+#if !defined(ASIO_HAS_DEDUCED_START_MEMBER_TRAIT)
+
+namespace asio {
+namespace traits {
+
+template <class Receiver, class... Senders>
+struct start_member<asio_ext::when_all::detail::operation_state<Receiver, Senders...>>
+{
+    ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+    ASIO_STATIC_CONSTEXPR(bool, is_noexcept = true);
+    typedef void result_type;
+};
+
+} // namespace traits
+} // namespace asio
+#endif // !defined(ASIO_HAS_DEDUCED_START_MEMBER_TRAIT)
+
+#if !defined(ASIO_HAS_DEDUCED_CONNECT_MEMBER_TRAIT)
+
+namespace asio {
+namespace traits {
+
+template <class... Senders, typename R>
+struct connect_member<asio_ext::when_all::detail::when_all_op<Senders...>, R>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = false);
+  typedef typename 
+      asio_ext::when_all::detail::operation_state<asio_ext::remove_cvref_t<R>, Senders...>
+      result_type;
+};
+
+} // namespace traits
+} // namespace asio
+
+#endif // !defined(ASIO_HAS_DEDUCED_CONNECT_MEMBER_TRAIT)
+
+#if !defined(ASIO_HAS_DEDUCED_SET_VALUE_MEMBER_TRAIT)
+
+namespace asio {
+namespace traits {
+
+template <typename Receiver, class... Senders, class... Values>
+struct set_value_member<asio_ext::when_all::detail::op_receiver<Receiver, Senders...>, void(Values...)>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = false);
+  typedef void result_type;
+};
+
+} // namespace traits
+} // namespace asio
+
+#endif // !defined(ASIO_HAS_DEDUCED_SET_VALUE_MEMBER_TRAIT)
+
+#if !defined(ASIO_HAS_DEDUCED_SET_ERROR_MEMBER_TRAIT)
+
+namespace asio {
+namespace traits {
+
+template <typename Receiver, class... Senders, typename E>
+struct set_error_member<asio_ext::when_all::detail::op_receiver<Receiver, Senders...>, E>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = true);
+  typedef void result_type;
+};
+
+} // namespace traits
+} // namespace asio
+
+#endif // !defined(ASIO_HAS_DEDUCED_SET_ERROR_MEMBER_TRAIT)
+
+#if !defined(ASIO_HAS_DEDUCED_SET_DONE_MEMBER_TRAIT)
+
+namespace asio {
+namespace traits {
+
+template <typename Receiver, class... Senders>
+struct set_done_member<asio_ext::when_all::detail::op_receiver<Receiver, Senders...>>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = true);
+  typedef void result_type;
+};
+
+} // namespace traits
+} // namespace asio
+
+#endif // !defined(ASIO_HAS_DEDUCED_SET_DONE_MEMBER_TRAIT)
--- test/when_all.cpp
+++ test/when_all.cpp
@@ -1,2 +1,32 @@
 #include <doctest/doctest.h>
+#include <asio_ext/just.hpp>
+#include <asio_ext/sync_wait.hpp>
+#include <asio_ext/transform.hpp>
 #include <asio_ext/when_all.hpp>
+
+using namespace asio::execution;
+
+template <typename F>
+auto lazy(F&& f) {
+    return transform(just(), (F&&)f);
+}
+
+TEST_CASE("single event when_all test")
+{
+    bool done1 = false;
+    sync_wait(when_all(
+        lazy([&] { done1 = true; })
+    ));
+    REQUIRE(done1 == true);
+}
+
+TEST_CASE("mutiple eveents when_all test")
+{
+    bool done1 = false, done2 = false;
+    sync_wait(when_all(
+        lazy([&] { done1 = true; }),
+        lazy([&] { done2 = true; })
+    ));
+    REQUIRE(done1 == true);
+    REQUIRE(done2 == true);
+}
